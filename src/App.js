30bfaa73457ede06b834b95d77b97f98cf698add
Sumant Shah <sumant.shah@gigacars.com>
2024-04-10 15:29:24 +0530

    routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import About from './components/About';
+import Contact from './components/Contact';
+import ErrorPage from './components/ErrorPage';
+import Home from './components/Home';
+import Login from './components/Login';
+import Logout from './components/Logout';
+import Navbar from './components/Navbar';
+import Signup from './components/Signup';
 import './styles/mainStyle.scss';
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-      
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/logout' element={<Logout />} />
+        <Route path='/*' element={<ErrorPage />} />
+      </Routes>
     </>
   );
 }
