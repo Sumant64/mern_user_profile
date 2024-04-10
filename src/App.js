3543ea9cec7e30a279e5eadbb418bd793233cea8
Sumant Shah <sumant.shah@gigacars.com>
2024-04-10 15:51:43 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { createContext, useReducer } from 'react';
 import About from './components/About';
 import Contact from './components/Contact';
 import ErrorPage from './components/ErrorPage';
@@ -8,20 +9,27 @@ import Navbar from './components/Navbar';
 import Signup from './components/Signup';
 import './styles/mainStyle.scss';
 import { Route, Routes } from "react-router-dom";
+import { initialState, reducer } from './reducer/UseReducer';
+
+export const UserContext = createContext();
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/contact' element={<Contact />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/signup' element={<Signup />} />
-        <Route path='/logout' element={<Logout />} />
-        <Route path='/*' element={<ErrorPage />} />
-      </Routes>
+      <UserContext.Provider value={{state, dispatch}} >
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/logout' element={<Logout />} />
+          <Route path='/*' element={<ErrorPage />} />
+        </Routes>
+      </UserContext.Provider>
     </>
   );
 }
