5fbc1f25cfc2e21a35800015369fd6d6f8f24f85
Sumant Shah <sumant.shah@gigacars.com>
2024-04-11 16:46:27 +0530

    404 error page

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const ErrorPage = () => {
   return (
-    <div>ErrorPage</div>
+    <>
+      <h1>404 Not Found</h1>
+      <NavLink to='/' >Back To HomePage</NavLink>
+    </>
   )
 }
 
