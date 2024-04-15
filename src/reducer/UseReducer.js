fe6de1e2eb5907a85519d524d3feaef6fc0c78b5
Sumant Shah <sumant.shah@gigacars.com>
2024-04-15 15:54:11 +0530

    reducer initial state fix

--- src/reducer/UseReducer.js
+++ src/reducer/UseReducer.js
@@ -1,4 +1,4 @@
-export const initialState = null;
+export const initialState = localStorage.getItem('token') ? true: null;
 
 export const reducer = (state, action) => {
     if(action.type === 'USER') {
